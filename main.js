e91f60fe1180d3bf9b016d762a33eb1677c1aabb
Nicholas Mutale <nicomutale@gmail.com>
2022-02-09 12:00:37 +0200

    update linter error

--- main.js
+++ main.js
@@ -1,72 +1,72 @@
-const mobileMenuOverlay = document.querySelector('.mobileMenu');
-const openMenuButton = document.querySelector('.mobileMenuIcon');
-const closeMenuButton = document.querySelector('.closeMenuIcon');
+const mobileMenuOverlay = document.querySelector(".mobileMenu");
+const openMenuButton = document.querySelector(".mobileMenuIcon");
+const closeMenuButton = document.querySelector(".closeMenuIcon");
 
 function toggleMenu() {
-    mobileMenuOverlay.style.display = 'block';
+  mobileMenuOverlay.style.display = "block";
 
-    closeMenuButton.style.display = 'block';
-    openMenuButton.style.display = 'none';
+  closeMenuButton.style.display = "block";
+  openMenuButton.style.display = "none";
 }
 
 function closeMenu() {
-    mobileMenuOverlay.style.display = 'none';
-    openMenuButton.style.display = 'block';
-    closeMenuButton.style.display = 'none';
+  mobileMenuOverlay.style.display = "none";
+  openMenuButton.style.display = "block";
+  closeMenuButton.style.display = "none";
 }
 
-openMenuButton.addEventListener('click', () => toggleMenu());
-closeMenuButton.addEventListener('click', () => closeMenu());
-//speaker section
-const speakerCard = document.querySelector('.SpeakerSection');
+openMenuButton.addEventListener("click", () => toggleMenu());
+closeMenuButton.addEventListener("click", () => closeMenu());
+
+const speakerCard = document.querySelector(".SpeakerSection");
 
 const speakersinfo = [
   {
-    Name: 'Mwiya Musokotwane',
-    Picture: './images/Mwiya1.jpg',
-    ImageAlt: 'Mwiya Musokotwane',
-    speakerTitle: 'CEO Thebe Investment',
-    Bio: 'Mwiya Musokotwane is the CEO of Zambia-based investment firm Thebe Investment Management.',
+    Name: "Mwiya Musokotwane",
+    Picture: "./images/Mwiya1.jpg",
+    ImageAlt: "Mwiya Musokotwane",
+    speakerTitle: "CEO Thebe Investment",
+    Bio: "Mwiya Musokotwane is the CEO of Zambia-based investment firm Thebe Investment Management.",
   },
   {
-    Name: 'Nolan Myers',
-    Picture: './images/nolan mayers.jpg',
-    ImageAlt: 'Nolan Myers',
-    speakerTitle: 'CEO DOUBLE GDP',
-    Bio: 'Nolan is a versatile leader with experience running customer success, professional services, operations, partnerships, and product.',
+    Name: "Nolan Myers",
+    Picture: "./images/nolan mayers.jpg",
+    ImageAlt: "Nolan Myers",
+    speakerTitle: "CEO DOUBLE GDP",
+    Bio: "Nolan is a versatile leader with experience running customer success, professional services, operations, partnerships, and product.",
   },
   {
-    Name: 'Doreen Okiri',
-    Picture: './images/Doreen.jpg',
-    ImageAlt: 'Doreen Okiri',
-    speakerTitle: 'Head of customer Success DOUBLE GDP',
-    Bio: 'Doreen is Specialised in Project management, software implementation and has extensive experience in SaaS customer success.',
+    Name: "Doreen Okiri",
+    Picture: "./images/Doreen.jpg",
+    ImageAlt: "Doreen Okiri",
+    speakerTitle: "Head of customer Success DOUBLE GDP",
+    Bio: "Doreen is Specialised in Project management, software implementation and has extensive experience in SaaS customer success.",
   },
   {
-    Name: 'Silumesii Maboshe',
-    Picture: './images/silumesii.jpg',
-    ImageAlt: 'Silumesii Maboshe',
-    speakerTitle: 'Co-Founder and Director',
-    Bio: 'Silumesii is a software developer, project manager, connector, researcher and has provided solutions to various customer  tech challenges.',
+    Name: "Silumesii Maboshe",
+    Picture: "./images/silumesii.jpg",
+    ImageAlt: "Silumesii Maboshe",
+    speakerTitle: "Co-Founder and Director",
+    Bio: "Silumesii is a software developer, project manager, connector, researcher and has provided solutions to various customer  tech challenges.",
   },
   {
-    Name: 'Ariel Camus',
-    Picture: './images/Ariel.jpg',
-    ImageAlt: 'Ariel Camus',
-    speakerTitle: 'Founder & CEO at Microverse',
-    Bio: 'Ariel believes that the place where you are born should not determine your opportunities in life.',
+    Name: "Ariel Camus",
+    Picture: "./images/Ariel.jpg",
+    ImageAlt: "Ariel Camus",
+    speakerTitle: "Founder & CEO at Microverse",
+    Bio: "Ariel believes that the place where you are born should not determine your opportunities in life.",
   },
   {
-    Name: 'Butler Shimaluwani',
-    Picture: './images/Butler.jpg',
-    ImageAlt: 'Butler Shimaluwani',
-    speakerTitle: 'Student at Microverse',
-    Bio: 'Butler is a full time student at Microverse, learning Full Stack Web Development.',
+    Name: "Butler Shimaluwani",
+    Picture: "./images/Butler.jpg",
+    ImageAlt: "Butler Shimaluwani",
+    speakerTitle: "Student at Microverse",
+    Bio: "Butler is a full time student at Microverse, learning Full Stack Web Development.",
   },
 ];
 
 function createSpeakerSection(cardData) {
-  speakerCard.innerHTML = ' ';
+  speakerCard.innerHTML = " ";
 
   cardData.forEach((card) => {
     const cardMaster = `
@@ -83,9 +83,8 @@ function createSpeakerSection(cardData) {
   </div>
   </div>
   `;
-    speakerCard.insertAdjacentHTML('beforeend', cardMaster);
+    speakerCard.insertAdjacentHTML("beforeend", cardMaster);
   });
 }
 
 createSpeakerSection(speakersinfo);
-
